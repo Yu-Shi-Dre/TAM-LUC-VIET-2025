af7d6792f5fa6133f4d929fdd970c6fd603dcf10
Nee Yu Shii <85595284+Yu-Shi-Dre@users.noreply.github.com>
2025-07-01 14:00:11 +0700

    Update Job.js

--- backend/models/Job.js
+++ backend/models/Job.js
@@ -1,1 +1,13 @@
+// models/Job.js
+const mongoose = require("mongoose");
+
+const jobSchema = new mongoose.Schema({
+  title: { type: String, required: true },
+  description: String,
+  location: String,
+  type: String,         // Chính thức / Thời vụ
+  salaryType: String    // Theo ngày / tháng / tuần
+}, { timestamps: true });
+
+module.exports = mongoose.model("Job", jobSchema);
 
