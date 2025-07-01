40b0da20a68d173102262022d6b0edce7ad5d349
Nee Yu Shii <85595284+Yu-Shi-Dre@users.noreply.github.com>
2025-07-01 13:57:30 +0700

    Update User.js

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,1 +1,25 @@
+// models/User.js
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  code: {
+    type: String,
+    required: true,
+    unique: true,
+    minlength: 10,
+    maxlength: 10
+  },
+  firebaseUID: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  role: {
+    type: String,
+    enum: ["admin", "staff"],
+    required: true
+  }
+}, { timestamps: true });
+
+module.exports = mongoose.model("User", userSchema);
 
